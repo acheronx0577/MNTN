f8e5d992212af56e01612796606f931e3d82aa54
AcheronX. <richardhuynh53@gmail.com>
2025-07-07 10:46:19 -0700

    feat: add favorites collection with auth rules

--- pocketbase/pb_migrations/1740000000_init_collections.js
+++ pocketbase/pb_migrations/1740000000_init_collections.js
@@ -37,3 +37,30 @@ migrate(
     app.save(hikes);
 
     const favorites = new Collection({
+      name: "favorites",
+      type: "base",
+      fields: [
+        {
+          name: "user",
+          type: "relation",
+          required: true,
+          collectionId: users.id,
+          cascadeDelete: true,
+          maxSelect: 1,
+        },
+        {
+          name: "hike",
+          type: "relation",
+          required: true,
+          collectionId: hikes.id,
+          cascadeDelete: true,
+          maxSelect: 1,
+        },
+      ],
+      listRule: '@request.auth.id != "" && user = @request.auth.id',
+      viewRule: '@request.auth.id != "" && user = @request.auth.id',
+      createRule: '@request.auth.id != "" && @request.data.user = @request.auth.id',
+      updateRule: '@request.auth.id != "" && user = @request.auth.id',
+      deleteRule: '@request.auth.id != "" && user = @request.auth.id',
+    });
+    app.save(favorites);
